337a434593534fc38880a1cccda3c23a96cbeb23
mot25 <76829487+mot25@users.noreply.github.com>
2022-03-27 16:00:10 +0200

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import AuthPages from './pages/AuthPages/AuthPages';
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <AuthPages />
     </div>
   );
 }
